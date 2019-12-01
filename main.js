dff5a9e3b18cd7e327b7fb9a9cb792080aaa7b9a
moogiecodes <mchoichoi@gmail.com>
2019-12-01 15:17:45 -0800

    Add remove meme button, form clears after submission

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ let memeCount = 0;
 let currentNode;
 // register event handler and initialize currentNode
 function start() {
+  var memeForm = document.getElementById("memeForm");
+  memeForm.addEventListener("submit", addMeme, false);
   document
-    .getElementById("memeForm")
-    .addEventListener("submit", addMeme, false);
+    .getElementById("removebtn")
+    .addEventListener("click", removeMeme, false);
   // initialize current node
-  // currentNode = document.getElementById("meme1"); // CHANGE ?
+  currentNode = document.getElementById("meme1");
 }
 window.addEventListener("load", start, false);
 // append div template to meme flexbox
@@ -21,11 +23,6 @@ function addMeme() {
   var memeUrl = document.getElementById("memeurl").value;
   var topTxt = document.getElementById("toptext-input").value;
   var btmTxt = document.getElementById("btmtext-input").value;
-  /*
-  var url = memeUrl.innerText;
-  var top = topTxt.innerText;
-  var btm = btmTxt.innerText;
-  */
   ++memeCount;
   let newMeme = document.createElement("div");
   newMeme.innerHTML = document.getElementById("memeTmplt").innerHTML;
@@ -35,6 +32,20 @@ function addMeme() {
   newMeme.children[1].innerText = topTxt; // set val of top text input from form
   newMeme.children[2].innerText = btmTxt; // set val of btm text input from form
   newMeme.id = "meme" + memeCount; // sets id of meme wrapper, can refer to delete later
-
-  //switchTo(newInput);
+  // clear form
+  memeForm.reset();
+  switchTo(newMeme);
+}
+// helper function that switches to currentNode
+function switchTo(newMeme) {
+  currentNode = newMeme;
+}
+// removes current node (last generated meme)
+function removeMeme() {
+  if (memeCount < 1) alert("There are no memes to remove.");
+  else {
+    var removeNode = document.getElementById("meme" + memeCount);
+    removeNode.remove();
+    --memeCount;
+  }
 }
