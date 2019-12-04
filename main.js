250fead4ea36a39137ba458d06a6a4c0d123c7b1
moogiecodes <mchoichoi@gmail.com>
2019-12-03 18:10:29 -0800

    Clean up JS

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 let memeCount = 0;
 let currentNode;
-// register event handler and initialize currentNode
+
+// register event handlers and initialize currentNode
 function start() {
   var memeForm = document.getElementById("memeForm");
   memeForm.addEventListener("submit", addMeme, false);
-  document
-    .getElementById("removebtn")
-    .addEventListener("click", removeMeme, false);
+  var removeBtn = document.getElementById("removebtn");
+  removeBtn.addEventListener("click", removeMeme, false);
   // initialize current node
   currentNode = document.getElementById("meme1");
 }
 window.addEventListener("load", start, false);
-// append div template to meme flexbox
+
+// append new meme div to flexbox using template
 function addMeme() {
   event.preventDefault();
   if (memeCount >= 10) {
@@ -32,14 +33,16 @@ function addMeme() {
   newMeme.children[1].innerText = topTxt; // set val of top text input from form
   newMeme.children[2].innerText = btmTxt; // set val of btm text input from form
   newMeme.id = "meme" + memeCount; // sets id of meme wrapper, can refer to delete later
-  // clear form
+  // clear form after submit
   memeForm.reset();
   switchTo(newMeme);
 }
-// helper function that switches to currentNode
+
+// helper function that switches current node to new meme created
 function switchTo(newMeme) {
   currentNode = newMeme;
 }
+
 // removes current node (last generated meme)
 function removeMeme() {
   if (memeCount < 1) alert("There are no memes to remove.");
